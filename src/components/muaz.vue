fea1f035de84d278a7231b5438051f76515b1ec2
Muaz Wazir <muazwazir@gmail.com>
2020-11-24 17:29:07 +0800

    repo

--- src/components/muaz.vue
+++ src/components/muaz.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <p>{{ msg2 }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: "muaz",
   props: {
     msg: String,
+    msg2: String,
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
